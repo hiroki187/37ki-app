9e2cb79ff526c47281716446822ce5b7566a9a8b
HirasawaMotoki <moto.perin@gmail.com>
2020-11-30 23:39:25 +0900

    a

--- graduate/src/components/App.js
+++ graduate/src/components/App.js
@@ -5,6 +5,8 @@ import Recommend from "./Recommend";
 import Profile from "./Profile";
 import Todo from "./Todo";
 import Header from "./Header";
+import Login from "./Login";
+import Signup from "./Signup";
 
 const App = () => {
   return (
@@ -14,6 +16,8 @@ const App = () => {
           <Header />
           <Switch>
             <Route path="/" exact component={Chatapp} />
+            <Route path="/login" exact component={Login} />
+            <Route path="/signup" exact component={Signup} />
             <Route path="/graduate/recommend" exact component={Recommend} />
             <Route path="/graduate/todo" exact component={Todo} />
             <Route path="/graduate/profile" exact component={Profile} />

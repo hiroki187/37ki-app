d909547389efa9dcb88b2e8870b6c2f37534522e
HirasawaMotoki <moto.perin@gmail.com>
2020-11-30 22:08:49 +0900

    リンク先に飛べる様にしまた先に飛べる様にしました

--- graduate/src/components/App.js
+++ graduate/src/components/App.js
@@ -1,7 +1,27 @@
 import React from "react";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+import Chatapp from "./Chatapp";
+import Recommend from "./Recommend";
+import Profile from "./Profile";
+import Todo from "./Todo";
+import Header from "./Header";
 
 const App = () => {
-  return <div>App</div>;
+  return (
+    <div className="ui container">
+      <BrowserRouter>
+        <div>
+          <Header />
+          <Switch>
+            <Route path="/" exact component={Chatapp} />
+            <Route path="/graduate/recommend" exact component={Recommend} />
+            <Route path="/graduate/todo" exact component={Todo} />
+            <Route path="/graduate/profile" exact component={Profile} />
+          </Switch>
+        </div>
+      </BrowserRouter>
+    </div>
+  );
 };
 
 export default App;
